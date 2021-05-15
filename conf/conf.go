fd789dc48fec0555d19d1d6c74d354e0d7adf974
jqiris <jieqiang1017@163.com>
2021-05-15 20:13:40 +0800

    周报工具

--- conf/conf.go
+++ conf/conf.go
@@ -3,24 +3,19 @@ package conf
 import (
 	"encoding/json"
 	"io/ioutil"
-
-	"github.com/sirupsen/logrus"
 )
 
 var (
 	config = new(Config)
-	logger = logrus.WithField("package", "conf")
 )
 
 func InitConf(filename string) error {
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
-		logger.Errorf("read file: %v error:%v", filename, err)
 		return err
 	}
 	err = json.Unmarshal(content, config)
 	if err != nil {
-		logger.Errorf("decode json error: %v", err)
 		return err
 	}
 	return nil
